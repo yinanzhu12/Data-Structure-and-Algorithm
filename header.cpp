8f3452040d2362c1e42f51f516a7e77444e4bedf
yinanzhu12 <welltempered.ivan@gmail.com>
2017-01-24 13:46:20 -0600

    add header

---
+++ header.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <vector>
+#include <set>
+#include <queue>
+#include <deque>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+typedef vector< vector<int> > matrix
+#define vi vector<int>
+#define ii pair<int, int>
+#define lop(i,a,b) for (i=a; i<=b; i++)
+#define rlop(i,a,b) for (i=b; i>=a; i--)
+#define trav(i,s) for (i=s.begin(); i!=s.end(); i++)
+#define all(s) (s).begin(),(s).end()
+#define pb push_back
+#define fill(s,v) memset(s,v,sizeof(s))
+
+const long double pi = acos(-1.0);
