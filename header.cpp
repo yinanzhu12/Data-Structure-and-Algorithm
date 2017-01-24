4117def51852cf433d912ab6a813dce0466bb87b
yinanzhu12 <welltempered.ivan@gmail.com>
2017-01-24 13:51:02 -0600

    update

--- header.cpp
+++ header.cpp
@@ -10,14 +10,21 @@
 
 using namespace std;
 
-typedef vector< vector<int> > matrix
-#define vi vector<int>
-#define ii pair<int, int>
+typedef vector<int> vi;
+typedef vector<vi> matrix;
+typedef pair<int, int> ii;
 #define lop(i,a,b) for (i=a; i<=b; i++)
 #define rlop(i,a,b) for (i=b; i>=a; i--)
-#define trav(i,s) for (i=s.begin(); i!=s.end(); i++)
+#define trav(c,i) for(typeof((c).begin() i = (c).begin(); i != (c).end(); i++) 
 #define all(s) (s).begin(),(s).end()
+#define isthere(c,x) ((c).find(x) != (c).end()) 
+#define cisthere(c,x) (find(all(c),x) != (c).end())
 #define pb push_back
 #define fill(s,v) memset(s,v,sizeof(s))
+#define sz(a) int((a).size()) 
 
 const long double pi = acos(-1.0);
+
+
+
+ 
