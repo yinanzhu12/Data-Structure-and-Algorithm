05cb70ff7544063cb8b94f7d729ff93b55fe885e
yinanzhu12 <welltempered.ivan@gmail.com>
2017-01-24 19:55:43 -0600

    update header

--- header.cpp
+++ header.cpp
@@ -1,9 +1,12 @@
 #include <cmath>
 #include <vector>
 #include <set>
+#include <map>
+#include <stack>
 #include <queue>
 #include <deque>
 #include <string>
+#include <fstream>
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
@@ -15,7 +18,7 @@ typedef vector<vi> matrix;
 typedef pair<int, int> ii;
 #define lop(i,a,b) for (i=a; i<=b; i++)
 #define rlop(i,a,b) for (i=b; i>=a; i--)
-#define trav(c,i) for(typeof((c).begin() i = (c).begin(); i != (c).end(); i++) 
+#define trav(c,i) for(typeof((c).begin()) i = (c).begin(); i != (c).end(); i++) 
 #define all(s) (s).begin(),(s).end()
 #define isthere(c,x) ((c).find(x) != (c).end()) 
 #define cisthere(c,x) (find(all(c),x) != (c).end())
@@ -26,5 +29,24 @@ typedef pair<int, int> ii;
 const long double pi = acos(-1.0);
 
 
+int main() {
+	/* for reading .txt file
+	ifstream infile;
+	ofstream outfile;
+	infile.open("input.txt");
+	cin.rdbuf(infile.rdbuf());
+	outfile.open("output.txt");
+	cout.rdbuf(outfile.rdbuf());*/
 
- 
+	/*for multiple test case
+	int t;
+	cin >> t;
+	lop(testcase, 1, t) {
+
+	}*/
+
+	/*output with k digits after decimal
+	cout<<setprecision(k)<<fixed;
+	*/
+	return 0;
+}
