4ee4e86d98d23a8fd6bd1ef3841ffdf739be197c
Yinan Zhu <welltempered.ivan@gmail.com>
2017-02-05 12:06:23 -0600

    Update header.cpp

--- header.cpp
+++ header.cpp
@@ -17,6 +17,7 @@ typedef vector<int> vi;
 typedef pair<int, int> ii;
 typedef vector<long> vl;
 typedef long long ll;
+typedef pair<long long, long long> llll;
 typedef vector<long long> vll;
 #define matrix(a) vector< vector<a> >
 #define sz(a) int((a).size()) 
