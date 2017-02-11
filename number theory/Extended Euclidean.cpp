dcaa8f6c35b5ec73d7f7a84b6a6d7d8066519c5c
Yinan Zhu <welltempered.ivan@gmail.com>
2017-02-11 15:46:42 -0600

    Update Extended Euclidean.cpp

--- number theory/Extended Euclidean.cpp
+++ number theory/Extended Euclidean.cpp
@@ -1,7 +1,7 @@
-long long extendedeuclidean(long long a, long long b) {
+long long euclidean(long long a, long long b) {
 	/*To compute gcd(a, b), return r[i].
 	To compute 1/a mod b provided gcd(a,b)=1, return s[i]
-	To compute solution to a*s+b*t=gcd(a,b), return s[k] and t[k]*/
+	To compute solution to a*s+b*t=gcd(a,b), return s[i] and t[i]*/
 	vector<long long> r = { a,b };
 	vector<long long> s = { 1,0 };
 	vector<long long> t = { 0,1 };
