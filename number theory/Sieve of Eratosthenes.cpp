3c4c0299bcf2694454b602e299d66d98c7e8095c
Yinan Zhu <welltempered.ivan@gmail.com>
2017-02-19 19:57:41 -0600

    Update Sieve of Eratosthenes.cpp

--- number theory/Sieve of Eratosthenes.cpp
+++ number theory/Sieve of Eratosthenes.cpp
@@ -5,18 +5,18 @@ int nextprime(vector<int>& sieve, int previous) {
 	int next = previous + 1;
 	while (sieve[next] == 0)next++;
 	int nextmultiple = next * 2;
-	while (nextmultiple <= sieve.size() - 1) {
+	while (nextmultiple <= sz(sieve) - 1) {
 		sieve[nextmultiple] = 0;
 		nextmultiple += next;
 	}
 	return next;
 }
 int main() {
-	int max=10000;
-	vector<int> sieve(max + 1, 1);
+	int inf=10000;
+	vector<int> sieve(inf + 1, 1);
 	sieve[1] = 0;
 	int newprime = 1;
-	while (newprime <= max) {
+	while (newprime <= inf) {
 		newprime = nextprime(sieve, newprime);
 	}
 	return 0;
