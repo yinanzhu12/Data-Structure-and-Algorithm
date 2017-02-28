19b66c14f4d8aba12e1524d5b6a2dc3d44f14f26
Yinan Zhu <welltempered.ivan@gmail.com>
2017-02-28 12:07:38 -0600

    Update trie.cpp

--- data structure/trie.cpp
+++ data structure/trie.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
+/*determine the alphabet needed to deal with*/
+const char start = 'a';
+const int range = 26;
+
 class trie {
-private:
-	/*determined by the alphabet list needed to deal with*/
-	const int start = 97;
-	const int range = 26;
 public:
 	int words;
 	int prefixes;
@@ -15,47 +15,33 @@ public:
 	trie() {
 		words = 0;
 		prefixes = 0;
-		edges = vector<trie*>(this->range, nullptr);
+		edges = vector<trie*>(range, nullptr);
 	}
 	void add(string& s) {
-		if (unsigned(s.length()) == 0) {
+		if (!sz(s)) {
 			this->words += 1;
 			return;
 		}
 		this->prefixes += 1;
-		int c = s[0] - this->start;
-		if (this->edges[c] == nullptr) {
-			this->edges[c] = new trie;
-		}
+		int c = s[0] - start;
+		if (this->edges[c] == nullptr) this->edges[c] = new trie;
 		(*(this->edges[c])).add(s.substr(1));
 		return;
 	}
 	int countword(string& s) {
-		if (unsigned(s.length()) == 0) {
-			return this->words;
-		}
+		if (!sz(s)) return this->words;
 		else {
-			int c = s[0]-this->start;
-			if (this->edges[c] == nullptr) {
-				return 0;
-			}
-			else {
-				return (*(this->edges[c])).countword(s.substr(1));
-			}
+			int c = s[0]-start;
+			if (this->edges[c] == nullptr) return 0;
+			else return (*(this->edges[c])).countword(s.substr(1))
 		}
 	}
 	int countprefix(string& p) {
-		if (unsigned(p.length()) == 0) {
-			return this->prefixes;
-		}
+		if (!sz(p)) return this->prefixes;
 		else {
-			int c = p[0] - this->start;
-			if (this->edges[c] == nullptr) {
-				return 0;
-			}
-			else {
-				return (*(this->edges[c])).countprefix(p.substr(1));
-			}
+			int c = p[0] - start;
+			if (this->edges[c] == nullptr) return 0;
+			else return (*(this->edges[c])).countprefix(p.substr(1));
 		}
 	}
 };
