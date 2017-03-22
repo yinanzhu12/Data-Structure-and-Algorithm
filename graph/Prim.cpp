6ba5958f15fe55c9a662bdc6c6164cd1f614b642
yinanzhu12 <welltempered.ivan@gmail.com>
2017-03-21 19:59:33 -0500

    update

--- graph/Prim.cpp
+++ graph/Prim.cpp
@@ -1,11 +1,44 @@
 #include <cmath>
-#include <fstream>
-#include <cstdio>
 #include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <iomanip>
+#include <numeric>
+
 using namespace std;
 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef pair<long long, long long> l4;
+typedef vector<long long> vll;
+typedef double db;
+typedef vector<double> vdb;
+typedef set<int> si;
+typedef set<long long> sll;
+#define matrix(a) vector< vector<a> >
+#define sz(a) int((a).size()) 
+#define lop(i,a,b) for (int i=a; i<=b; i++)
+#define vlop(i,v) lop(i,0,sz(v)-1)
+#define vlop1(i,v) lop(i,1,sz(v)-1)
+#define rlop(i,a,b) for (int i=b; i>=a; i--)
+#define vrlop(i,v) rlop(i,0,sz(v)-1)
+#define vrlop1(i,v) rlop(i,1,sz(v)-1)
+#define printv(i,v) vlop(i,v)cout<<v[i]<<" "
+#define all(s) (s).begin(),(s).end()
+#define isthere(c,x) ((c).find(x) != (c).end())
+#define pb push_back
+#define enter cout<<'\n'
+
+/*must be greater than any edge's weight*/
+const int inf = 100001;
 
 class edge {
 public:
@@ -17,108 +50,63 @@ public:
 	}
 };
 
-class vertice {
-public:
-	int v;
-	int key;
-	vertice(int a, int b) {
-		v = a;
-		key = b;
-	}
-	vertice() {
-		v = 0;
-		key = 0;
-	}
-};
-
-void maintain(vector<vertice>& h, int root) {
-	int left = 2 * root;
-	int right = 2 * root + 1;
-	vertice t;
-	if (left>h.size()-1)return;
-	else if (left == h.size()-1) {
-		if (h[left].key<h[root].key) {
-			swap(h[root],h[left]);
-		}
-		else return;
-	}
+void initialize(int start, vi& stree, vi& level, int l, int u) {
+	if (sz(stree) - 1 < start)stree.resize(start + 1);
+	if (l == u)stree[start] = l;
 	else {
-		if (h[root].key<=h[left].key && h[root].key<=h[right].key)return;
-		else if (h[right].key>h[left].key) {
-			swap(h[root], h[left]);
-			maintain(h, left);
-		}
-		else {
-			swap(h[root], h[right]);
-			maintain(h, right);
-		}
+		initialize(2 * start, stree, level, l, (u + l) / 2);
+		initialize(2 * start + 1, stree, level, (u + l) / 2 + 1, u);
+		if (level[stree[start * 2]] < level[stree[start * 2 + 1]])stree[start] = stree[start * 2];
+		else stree[start] = stree[start * 2 + 1];
 	}
 	return;
 }
-
-vertice extractmin(vector<vertice>& h) {
-	if (h.size() == 1)return v();
-	swap(h[1],h[h.size()-1]);
-	vertice t = h[h.size() - 1];
-	heap.pop_back();
-	maintain(h, 1);
-	return t;
-}
-
-void changekey(vector<vertice>& h, int change, int newkey) {
-	h[change].key = newkey;
-	vertice t;
-	while (change>1 && h[change].key<h[change / 2].key) {
-		swap(h[change],h[change/2]);
-		change /= 2;
+void updatetree(int start, vi& stree, vi& level, int index, int value, int l, int u) {
+	if (l == u) {
+		level[index] = value;
+		return;
 	}
+	if (index <= (l + u) / 2)updatetree(2 * start, stree, level, index, value, l, (l + u) / 2);
+	else updatetree(2 * start + 1, stree, level, index, value, (l + u) / 2 + 1, u);
+	if (level[stree[start * 2]] < level[stree[start * 2 + 1]])stree[start] = stree[start * 2];
+	else stree[start] = stree[start * 2 + 1];
 	return;
 }
 
-int main() {
-	ifstream cin("input.txt");
-	int n;
-	cin >> n;
-	int m;
-	cin >> m;
-	int inf = 100001;
-	vector< vector<edge> > graph(n + 1);
-	vector<vertice> heap;
-	heap.push_back(vertice(0, 0));
-	for (int i = 0; i<m; i++) {
-		int a;
-		int b;
-		int c;
-		cin >> a >> b >> c;
-		graph[a].push_back(edge(b, c));
-		graph[b].push_back(edge(a, c));
-	}
-	int s;
-	cin >> s;
-	heap.push_back(vertice(s, 0));
-	vector<int> intree(n+1,0);
-	for (int i = 1; i <= n; i++) {
-		if (i != s) {
-			heap.push_back(vertice(i, inf));
-		}
-	}
-	long long int sum = 0;
-	while (heap.size() != 1) {
-		vertice newv = extractmin(heap);
-		cout << newv.v<<" "<<newv.key<< endl;
-		sum += newv.key;
-		intree[newv.v] = true;
-		for (int i = 0; i<graph[newv.v].size(); i++) {
-			if (!intree[graph[newv.v][i].v]) {
-				for (int j = 1; j<heap.size(); j++) {
-					if (heap[j].v == graph[newv.v][i].v && heap[j].key>graph[newv.v][i].w) {
-						changekey(heap.begin(), j, graph[newv.v][i].w);
-						break;
-					}
-				}
+void prim(matrix(edge)& alist,matrix(edge)& tree) {
+	int n = sz(alist) - 1;
+	vi intree(n + 1, 0), key(n + 1, inf), stree,link(n+1);
+	/*start search from node 1*/
+	key[1] = 0;
+	initialize(1, stree, key, 0, n);
+	lop(i, 1, n) {
+		int node = stree[1];
+		updatetree(1, stree, key, node, inf, 0, n);
+		intree[node] = 1;
+		tree[node].pb(edge(link[node], key[node]));
+		tree[link[node]].pb(edge(node,key[node]));
+		vlop(j, alist[node]) {
+			if (!intree[alist[node][j].v] && key[alist[node][j].v] > alist[node][j].w) {
+				updatetree(1, stree, key, alist[node][j].v, alist[node][j].w, 0, n);
+				link[alist[node][j].v] = node;
 			}
 		}
 	}
-	cout << sum;
+	return;
+}
+
+/*use Prim algorithm to find the MST of a weighted undirected connected graph 
+with n nodes (indexed from 1) and m edges, store the tree in another adjacency list*/
+int main() {
+	int n,m;
+	cin >> n>>m;
+	matrix(edge) alist(n + 1),tree(n+1);
+	lop(i,1,m) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		alist[a].push_back(edge(b, c));
+		alist[b].push_back(edge(a, c));
+	}
+	prim(alist, tree);
 	return 0;
 }
---
+++ graph/topological sort.cpp
@@ -0,0 +1,73 @@
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <numeric>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef pair<long long, long long> l4;
+typedef vector<long long> vll;
+typedef double db;
+typedef vector<double> vdb;
+typedef set<int> si;
+typedef set<long long> sll;
+#define matrix(a) vector< vector<a> >
+#define sz(a) int((a).size()) 
+#define lop(i,a,b) for (int i=a; i<=b; i++)
+#define vlop(i,v) lop(i,0,sz(v)-1)
+#define vlop1(i,v) lop(i,1,sz(v)-1)
+#define rlop(i,a,b) for (int i=b; i>=a; i--)
+#define vrlop(i,v) rlop(i,0,sz(v)-1)
+#define vrlop1(i,v) rlop(i,1,sz(v)-1)
+#define printv(i,v) vlop(i,v)cout<<v[i]<<" "
+#define all(s) (s).begin(),(s).end()
+#define isthere(c,x) ((c).find(x) != (c).end())
+#define pb push_back
+#define enter cout<<'\n'
+
+void topsort(matrix(int)& edge, matrix(int)& alist, vi& indeg, vi& sorted) {
+	stack<int> open;
+	vlop(i, ideg) {
+		if (!indeg[i])open.push(i);
+	}
+	while (!open.empty()) {
+		int node = open.top();
+		sorted.pb(node);
+		open.pop();
+		vlop(i, alist[node]) {
+			indeg[alist[node][i]]--;
+			if (!indeg[alist[node][i]])open.push(alist[node][i]);
+		}
+	}
+	return;
+}
+
+/*give the topological sort of a DAG with n nodes and m edges*/
+int main() {
+	int n, m;
+	cin >> n >> m;
+	/*nodes of graph indexed from 1*/
+	matrix(int) edge(m, vi(2)), alist(n+1);
+	vi indeg(n+1, 0);
+	vlop(i, edges) {
+		cin >> edge[i][0] >> edge[i][1];
+		indeg[edge[i][1]]++;
+		alist[edge[i][0]].pb(edge[i][1]);
+	}
+	/*store the sorted nodes*/
+	vi sorted;
+	tosort(edge, alist, indeg, sorted);
+	return 0;
+}
