3875286cabbeac3660dbb1c007f5b9fd2ce39a6e
yinanzhu12 <welltempered.ivan@gmail.com>
2017-04-08 17:23:47 -0500

    update

---
+++ array/trivia in binary search.cpp
@@ -0,0 +1,69 @@
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <numeric>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef long long ll;
+typedef pair<long long, long long> l4;
+typedef vector<long long> vll;
+typedef double db;
+typedef vector<double> vdb;
+typedef pair<double, double> dd;
+typedef set<int> si;
+typedef set<long long> sll;
+#define fi first
+#define se second
+#define matrix(a) vector< vector<a> >
+#define sz(a) int((a).size()) 
+#define lop(i,a,b) for (int i=a; i<=b; i++)
+#define vlop(i,v) lop(i,0,sz(v)-1)
+#define vlop1(i,v) lop(i,1,sz(v)-1)
+#define rlop(i,a,b) for (int i=b; i>=a; i--)
+#define vrlop(i,v) rlop(i,0,sz(v)-1)
+#define vrlop1(i,v) rlop(i,1,sz(v)-1)
+#define printv(i,v) vlop(i,v)cout<<v[i]<<" "
+#define printv1(i,v) vlop1(i,v)cout<<v[i]<<" "
+#define all(s) (s).begin(),(s).end()
+#define pb push_back
+#define enter cout<<'\n'
+#define lb(i,v) int(lower_bound(all(v),i)-v.begin())
+#define ub(i,v) int(upper_bound(all(v),i)-v.begin())
+
+
+int countbetween(vi& v, int l, int r) {
+	if (l > r || !sz(v))return 0;
+	return ub(r, v) - lb(l, v);
+}
+
+int main() {
+	/*general binary search procejure*/
+	int l, u;
+	int m = (l + u) / 2;
+	bool check;
+	while (l != u) {
+		check = f(m);
+		if (check) u = m;
+		else l = m + 1;
+	}
+	
+	
+	/*in a SORTED array, count how many elements satisfy v[i]<=r && v[i]>=l*/
+	int l, r;
+	vi v;
+	sort(all(v));
+	countbetween(v, l, r);
+	return 0;
+}
