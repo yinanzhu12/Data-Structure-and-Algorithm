5f3c30359846f6eff6d5ab8576b3cda986b39ed0
Yinan Zhu <welltempered.ivan@gmail.com>
2017-04-10 00:57:12 -0500

    Update Knuth-Morris-Pratt.cpp

--- Knuth-Morris-Pratt.cpp
+++ Knuth-Morris-Pratt.cpp
@@ -52,6 +52,25 @@ void failure(vi& f, string& s) {
 	return;
 }
 
+void findsubstring(vi& st,vi& ft,string& s,string& t){
+    int success=0;
+    vlop(i,s){
+        if(success==sz(t))success=ft.back();
+        if(s[i]==t[success])success++;
+        else{
+            while(true){
+                if(!success)break;
+                success=ft[success-1];
+                if(s[i]==t[success]){
+                    success++;
+                    break;
+                }
+            }
+        }
+        st[i]=success;
+    }
+}
+
 void dfs(matrix(int)& alist, int root, vi& count, vi& subtreesum) {
 	vlop(i, alist[root]) {
 		dfs(alist, alist[root][i], count,subtreesum);
@@ -71,19 +90,27 @@ void findprefix(int l,vi& f, vi& result){
 }
 
 int main(){
-	string s;
+	string s,t;
 	vi f(sz(s));
 	f[0]=0;
 	/*compute failure function, s start from index 0
-	f[i] is the maximum LENGTH the suffix ending in s[i] can be realized as prefix, EXCEPT the string (s[0]...s[i]) itself*/
-	failure(f, s);
+	fs[i] is the maximum LENGTH the suffix ending in s[i] can be realized as prefix of s, EXCEPT the string (s[0]...s[i]) itself*/
+	failure(fs, s);
+	failure(ft,t);
+	vi st(sz(s));
+	/*st[i]=j iff j is the length of the longest prefix of t that can be realized as a substring of s ending at s[i]*/
+	findsubstring(st,ft,s,t);
+	
+	/*applications*/
+	vi result;
+	/*find all the t such that (s[0]...s[t-1]) can be realized as a suffix ending at s[l]*/
+	findprefix(l,f,result);
 	vlop(i,f)count[f[i]]++;
+	
 	/*build a DIRECTED tree rooted at 0, with vertice 0...sz(s), edge f[i]->i*/
 	matrix(int) alist(sz(s)+1);
 	vlop(i,f) alist[f[i]].pb(i+1);
 	/*subtreesum[i]+1 will be the time s[0]...s[i-1] appears in s as a substring*/
 	dfs(alist,0,count,subtreesum);
-        vi result;
-	/*this procedure find all the t such that (s[0]...s[t-1]) can be realized as a suffix ending at s[l]*/
-	findprefix(l,f,result);
+        
 }
