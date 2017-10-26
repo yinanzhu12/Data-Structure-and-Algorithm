72959b261a4eb0f197e02ba1f169aa59a7d91eb9
Yinan Zhu <welltempered.ivan@gmail.com>
2017-10-25 20:48:58 -0500

    Update Dijistra.cpp

--- graph/single source shortest path/Dijistra.cpp
+++ graph/single source shortest path/Dijistra.cpp
@@ -40,8 +40,10 @@ typedef set<long long> sll;
 
 /*must be greater than the SUM of all edges*/
 const int inf = 1000000000;
-
-void initialize(int start, vi& stree, vi& level, int l, int u) {
+matrix(int) alist;
+vi d;
+template <typename T>
+void initialize(int start, vi& stree, vector<T>& level, int l, int u) {
 	if (sz(stree) - 1 < start)stree.resize(start + 1);
 	if (l == u)stree[start] = l;
 	else {
@@ -52,8 +54,8 @@ void initialize(int start, vi& stree, vi& level, int l, int u) {
 	}
 	return;
 }
-
-void updatetree(int start, vi& stree, vi& level, int index, int value, int l, int u) {
+template <typename T> 
+void updatetree(int start, vi& stree, vector<T>& level, int index, T value, int l, int u) {
 	if (l == u) {
 		level[index] = value;
 		return;
@@ -65,38 +67,38 @@ void updatetree(int start, vi& stree, vi& level, int index, int value, int l, in
 	return;
 }
 
-void djs(matrix(ii)& alist,vi& distance,int source) {
+void djs(int source) {
 	int n = sz(alist) - 1,node;
 	vi visited(n + 1, 0), stree, key(n + 1,inf);
 	key[source] = 0;
-	initialize(1, stree, key, 0, n);
+	initialize<int>(1, stree, key, 0, n);
 	lop(i, 1, n) {
 		node = stree[1];
 		if (key[node] == inf)break;
-		distance[node] = key[node];
-		updatetree(1, stree, key, node, inf, 0, n);
+		d[node] = key[node];
+		updatetree<int>(1, stree, key, node, inf, 0, n);
 		visited[node] = 1;
-		vlop(j, alist[node]) {
-			if (!visited[alist[node][j].fi] && key[alist[node][j].fi] > distance[node]+alist[node][j].se)updatetree(1, stree, key, alist[node][j].fi, alist[node][j].se+distance[node], 0, n);
+		for(auto child: alist[node]) {
+			if (!visited[child.fi] && key[child.fi] > d[node]+child.se)updatetree<int>(1, stree, key, child.fi, child.se+d[node], 0, n);
 		}
 	}
 	return;
 }
 
-int singledjs(matrix(ii)& alist,int source,int dest) {
+int singledjs(int dest) {
 	int n = sz(alist) - 1,arrive, node;
 	vi visited(n + 1, 0), stree, key(n + 1,inf);
 	key[source] = 0;
-	initialize(1, stree, key, 0, n);
+	initialize<int>(1, stree, key, 0, n);
 	lop(i, 1, n) {
 		node = stree[1];
 		if (key[node] == inf)break;
 		if(node==dest)return key[node];
 		arrive = key[node];
-		updatetree(1, stree, key, node, inf, 0, n);
+		updatetree<int>(1, stree, key, node, inf, 0, n);
 		visited[node] = 1;
-		vlop(j, alist[node]) {
-			if (!visited[alist[node][j].fi] && key[alist[node][j].fi] > arrive+alist[node][j].se)updatetree(1, stree, key, alist[node][j].fi, alist[node][j].se+arrive, 0, n);
+		for(auto child: alist[node]) {
+			if (!visited[child.fi] && key[child.fi] > arrive+child.se)updatetree<int>(1, stree, key, child.fi, child.se+arrive, 0, n);
 		}
 	}
 	return -1;
@@ -107,10 +109,10 @@ in an DIRECTED NONNEGATIVE weighted graph with nodes 1,...,n and m edges*/
 int main() {
 	int n, m;
 	cin >> n >> m;
-	matrix(ii) alist(n + 1);
-	vi distance(n+1,-1);
+	alist.resize(n + 1);
+	d.resize(n+1,-1);
 	int source,dest;
-	djs(alist, distance,source);/*distance[i] is the length of shortest paht from source to i. -1 if not reachable*/
-	singledjs(alist,source,dest);/*return only the shortest path to a single destination, if not reachable, return -1*/
+	djs(source);/*distance[i] is the length of shortest paht from source to i. -1 if not reachable*/
+	singledjs(source,dest);/*return only the shortest path to a single destination, if not reachable, return -1*/
 	return 0;
 }
