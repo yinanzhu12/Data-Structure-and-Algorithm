b7c28c16e78aff4e3a2372c2396987cedc69808c
Yinan Zhu <welltempered.ivan@gmail.com>
2017-11-19 11:55:44 -0600

    Update grid to graph.cpp

--- graph/grid to graph.cpp
+++ graph/grid to graph.cpp
@@ -41,35 +41,35 @@ typedef set<long long> sll;
 #define enter cout<<'\n'
 #define lb(i,v) int(lower_bound(all(v),i)-v.begin())
 #define ub(i,v) int(upper_bound(all(v),i)-v.begin())
-
-int tonode(int i, int j, int m) {
+int n0,n1,n,m;
+int tonode(int i, int j) {
 	return (i - 1)*m + j;
 }
 
 int main() {
 	/*turn a grid with row 1,...,n, coloumn 1,...,m into an undirected graph where two adjacent grid are connected
 	iff both are marked zero*/
-	matrix(int) alist(n*m + 1),grid(n+1,vi(m+1));/*grid[0][i]=1,grid[i][0]=1*/
-	lop(i, 1, n - 1) {
-		lop(j, 1, m - 1) {
-			if (!realgrid[i][j]) {
-				int n0 = tonode(i, j, m);
-				if (!realgrid[i - 1][j]) {
+	matrix(int) alist(n*m + 1),grid(n+2,vi(m+2));
+	lop(i, 1, n ) {
+		lop(j, 1, m ) {
+			if (!grid[i][j]) {
+				n0 = tonode(i, j, m);
+				if (!grid[i - 1][j]&&i-1>=1) {
 					int n1 = tonode(i - 1, j, m);
 					alist[n1].pb(n0);
 					alist[n0].pb(n1);
 				}
-				if (!realgrid[i + 1][j]) {
+				if (!grid[i + 1][j]&&i+1<=n) {
 					int n1 = tonode(i + 1, j, m);
 					alist[n1].pb(n0);
 					alist[n0].pb(n1);
 				}
-				if (!realgrid[i][j - 1]) {
+				if (!grid[i][j - 1]&&j-1>=0) {
 					int n1 = tonode(i, j - 1, m);
 					alist[n1].pb(n0);
 					alist[n0].pb(n1);
 				}
-				if (!realgrid[i][j + 1]) {
+				if (!grid[i][j + 1]&&j+1<=m) {
 					int n1 = tonode(i, j + 1, m);
 					alist[n1].pb(n0);
 					alist[n0].pb(n1);
